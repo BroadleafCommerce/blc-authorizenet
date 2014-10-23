6cad8dd8b8ac03d0d66c977158ea1fef6770a2c6
Phillip Verheyden <pverheyden@gmail.com>
2014-10-22 22:43:30 -0500

    Fix a bug where the order was not checked when Authorize.net invokes Broadleaf to process the payment. This is required because there is no other way to prevent user error (like closing the browser/stopping the request) to guarantee that the appropriate things happen when a card is charged successfullly/unsuccessfully

--- src/main/java/org/broadleafcommerce/vendor/authorizenet/web/controller/BroadleafAuthorizeNetController.java
+++ src/main/java/org/broadleafcommerce/vendor/authorizenet/web/controller/BroadleafAuthorizeNetController.java
@@ -17,40 +17,38 @@
  * limitations under the License.
  * #L%
  */
+
 package org.broadleafcommerce.vendor.authorizenet.web.controller;
 
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.util.Map;
-import java.util.Set;
-
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import net.authorize.AuthNetField;
 import net.authorize.sim.Result;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.broadleafcommerce.common.payment.PaymentType;
+import org.broadleafcommerce.common.payment.dto.PaymentRequestDTO;
 import org.broadleafcommerce.common.payment.dto.PaymentResponseDTO;
 import org.broadleafcommerce.common.payment.service.PaymentGatewayConfiguration;
+import org.broadleafcommerce.common.payment.service.PaymentGatewayTransactionService;
 import org.broadleafcommerce.common.payment.service.PaymentGatewayWebResponsePrintService;
 import org.broadleafcommerce.common.payment.service.PaymentGatewayWebResponseService;
 import org.broadleafcommerce.common.vendor.service.exception.PaymentException;
 import org.broadleafcommerce.common.web.payment.controller.PaymentGatewayAbstractController;
 import org.broadleafcommerce.payment.service.gateway.AuthorizeNetConfiguration;
 import org.broadleafcommerce.vendor.authorizenet.service.payment.AuthorizeNetCheckoutService;
-import org.broadleafcommerce.vendor.authorizenet.service.payment.AuthorizeNetGatewayType;
-import org.broadleafcommerce.vendor.authorizenet.service.payment.type.MessageConstants;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author Chad Harchar (charchar)
@@ -66,14 +64,17 @@ public class BroadleafAuthorizeNetController extends PaymentGatewayAbstractContr
     @Resource(name = "blAuthorizeNetWebResponseService")
     protected PaymentGatewayWebResponseService paymentGatewayWebResponseService;
 
-    @Resource(name="blAuthorizeNetCheckoutService")
+    @Resource(name = "blAuthorizeNetCheckoutService")
     protected AuthorizeNetCheckoutService authorizeNetCheckoutService;
-    
+
     @Resource(name = "blAuthorizeNetConfiguration")
     protected AuthorizeNetConfiguration configuration;
 
     @Resource(name = "blPaymentGatewayWebResponsePrintService")
     protected PaymentGatewayWebResponsePrintService webResponsePrintService;
+    
+    @Resource(name = "blAuthorizeNetTransactionService")
+    protected PaymentGatewayTransactionService transactionService;
 
     @Override
     public String getGatewayContextKey() {
@@ -82,22 +83,15 @@ public class BroadleafAuthorizeNetController extends PaymentGatewayAbstractContr
 
     @Override
     public void handleProcessingException(Exception e, RedirectAttributes redirectAttributes) throws PaymentException {
-        if (LOG.isTraceEnabled()) {
-            LOG.trace("A Processing Exception Occurred for " + GATEWAY_CONTEXT_KEY +
-                    ". Adding Error to Redirect Attributes."+ e.getMessage());
-        }
-
-        redirectAttributes.addAttribute(PAYMENT_PROCESSING_ERROR, getProcessingErrorMessage());
+        // Don't do anything here because there isn't enough information in the given parameters to actually
+        // void the payment that we need to void the payment
+        throw new PaymentException(e);
     }
 
     @Override
     public void handleUnsuccessfulTransaction(Model model, RedirectAttributes redirectAttributes,
             PaymentResponseDTO responseDTO) throws PaymentException {
-        if (LOG.isTraceEnabled()) {
-            LOG.trace("A Processing Exception Occurred for " + GATEWAY_CONTEXT_KEY +
-                    ". Adding Error to Redirect Attributes.");
-        }
-
+        LOG.trace("A Processing Exception Occurred for " + GATEWAY_CONTEXT_KEY + ". Adding Error to Redirect Attributes.");
         redirectAttributes.addAttribute(PAYMENT_PROCESSING_ERROR, getProcessingErrorMessage());
     }
 
@@ -112,55 +106,89 @@ public class BroadleafAuthorizeNetController extends PaymentGatewayAbstractContr
     }
 
     @Override
-    @RequestMapping(value = "/return")
-    public String returnEndpoint(Model model, HttpServletRequest request, RedirectAttributes redirectAttributes,
-            Map<String, String> pathVars) throws PaymentException {
-        return super.process(model, request, redirectAttributes);
-    }
-
-    @Override
     @RequestMapping(value = "/error", method = RequestMethod.GET)
     public String errorEndpoint(Model model, HttpServletRequest request, RedirectAttributes redirectAttributes,
             Map<String, String> pathVars) throws PaymentException {
-        redirectAttributes.addAttribute(PAYMENT_PROCESSING_ERROR,
-                request.getParameter(PAYMENT_PROCESSING_ERROR));
+        redirectAttributes.addAttribute(PAYMENT_PROCESSING_ERROR, request.getParameter(PAYMENT_PROCESSING_ERROR));
         return getOrderReviewRedirect();
     }
 
+    @Override
+    @RequestMapping(value = "/return", method = RequestMethod.GET)
+    public String returnEndpoint(Model model, HttpServletRequest request,
+            final RedirectAttributes redirectAttributes,
+            @PathVariable Map<String, String> pathVars) throws PaymentException {
+        PaymentResponseDTO responseDTO = getWebResponseService().translateWebResponse(request);
+        String orderNumber = lookupOrderNumberFromOrderId(responseDTO);
+        return getConfirmationViewRedirect(orderNumber);
+    }
+
+    /**
+     * <p>
+     * Invoked by Authorize.net after taking the user's credit card. This should process the payment and add it to the order
+     * as well as check out the order if configured.
+     * 
+     * <p>
+     * At this point in the lifecyle of taking payment, the card has already been charged. If there is a problem checking
+     * out the order, we need to communicate that explicitly up to Authorize.net via a VOID transaction. Otherwise, the user
+     * will be left with a non-submitted Order but their card will still be charged.
+     * 
+     * @param request
+     * @param response
+     * @param model
+     * @return
+     */
     @RequestMapping(value = "/process", method = RequestMethod.POST, produces = "text/html")
-    public @ResponseBody String relay(HttpServletRequest request, HttpServletResponse response, Model model) throws NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException, BroadleafAuthorizeNetException {
+    public @ResponseBody String process(HttpServletRequest request,
+            HttpServletResponse response,
+            RedirectAttributes redirectAttributes,
+            Model model) throws PaymentException {
         LOG.debug("Authorize URL request - " + request.getRequestURL().toString());
         LOG.debug("Authorize Request Parameter Map (params: [" + requestParamToString(request) + "])");
-
-        PaymentResponseDTO responseDTO = new PaymentResponseDTO(PaymentType.CREDIT_CARD,
-                AuthorizeNetGatewayType.AUTHORIZENET)
-                .rawResponse(webResponsePrintService.printRequest(request));
-
         Result result = Result.createResult(configuration.getLoginId(), configuration.getMd5Key(), request.getParameterMap());
+        
+        boolean error = false;
+        String returnUrl = "";
+        try {
+            // Process the payment on the Broadleaf order side
+            returnUrl = super.process(model, request, redirectAttributes);
+        } catch (Exception e) {
+            error = true;
+        }
+        
+        // This is a hacky way to do this but has to be done because the handleProcessingException does not give enough
+        // information to actually be useful in doing what I need. Ideally this logic would go in handleProcessingException
+        if (error || getErrorViewRedirect().equals(returnUrl)) {
+            PaymentResponseDTO responseDTO = getWebResponseService().translateWebResponse(request);
+            PaymentRequestDTO voidRequest = responseToVoidRequest(responseDTO);
+            transactionService.voidPayment(voidRequest);
+            
+            return authorizeNetCheckoutService.buildRelayResponse(configuration.getErrorUrl(), result);
+        }
 
-        boolean approved = false;
-        if (result.getResponseCode().toString().equals("APPROVED")) {
-            approved = true;
+        return authorizeNetCheckoutService.buildRelayResponse(configuration.getConfirmUrl(), result);
+    }
+    
+    /**
+     * Builds a request with enough information to void the given response from the gateway
+     * 
+     * @param response
+     * @return
+     */
+    protected PaymentRequestDTO responseToVoidRequest(PaymentResponseDTO response) {
+        PaymentRequestDTO request = new PaymentRequestDTO()
+            .transactionTotal(response.getAmount().toString());
+        
+        for (Entry<String, String> field : response.getResponseMap().entrySet()) {
+            request.additionalField(field.getKey(), field.getValue());
         }
         
-        String tps = authorizeNetCheckoutService.createTamperProofSeal(result.getResponseMap().get(MessageConstants.BLC_CID), result.getResponseMap().get(MessageConstants.BLC_OID));
-        responseDTO.valid(tps.equals(result.getResponseMap().get(MessageConstants.BLC_TPS)));
-        
-        if (approved && responseDTO.isValid()){
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("Transaction success for order " + result.getResponseMap().get(AuthNetField.X_TRANS_ID.getFieldName()));
-                LOG.debug("Response for Authorize.net to relay to client: ");
-                LOG.debug(authorizeNetCheckoutService.buildRelayResponse(configuration.getConfirmUrl(), result));
-            }
-            return authorizeNetCheckoutService.buildRelayResponse(configuration.getConfirmUrl(), result);
-        }
-        
-        return authorizeNetCheckoutService.buildRelayResponse(configuration.getErrorUrl(), result);
+        return request;
     }
 
     protected String requestParamToString(HttpServletRequest request) {
         StringBuffer requestMap = new StringBuffer();
-        for (String key : (Set<String>)request.getParameterMap().keySet()) {
+        for (String key : (Set<String>) request.getParameterMap().keySet()) {
             requestMap.append(key + ": " + request.getParameter(key) + ", ");
         }
         return requestMap.toString();
